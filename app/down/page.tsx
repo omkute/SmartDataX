7fe7c421769fe045c41fb764d67a39e5b480830c
omkute <omkute636@gmail.com>
2025-08-23 20:05:51 +0530

    added ppt download feature

---
+++ app/down/page.tsx
@@ -0,0 +1,11 @@
+import PPTDownload from '@/components/costom/Pptdownload'
+
+function DownloadPage() {
+  return (
+    <div>
+        <PPTDownload/>
+    </div>
+  )
+}
+
+export default DownloadPage
---
+++ components/costom/Pptdownload.tsx
@@ -0,0 +1,24 @@
+"use client";
+import React from "react";
+
+function PPTDownload() {
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = "https://drive.google.com/uc?export=download&id=1ZuqXFre4yOvITff_XOK_fKxVI5k2u6lv"; 
+    link.download = "SmartDataX.pptx";
+    link.click();
+  };
+
+  return (
+    <div className="flex justify-center items-center p-6">
+      <button
+        onClick={handleDownload}
+        className="px-6 py-3 bg-green-600 text-white rounded-2xl shadow-md hover:bg-green-700 transition"
+      >
+        📥 Download PPT
+      </button>
+    </div>
+  );
+}
+
+export default PPTDownload;
