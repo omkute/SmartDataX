2ad17ac56dd66340fdf63555687d8b0b9b3ef3ad
omkute <omkute636@gmail.com>
2025-08-24 01:01:41 +0530

    path issue fixxed

--- components/costom/iconSlide.tsx
+++ components/costom/iconSlide.tsx
@@ -4,81 +4,37 @@ import { motion } from "framer-motion";
 
 type ToolItem = {
   name: string;
-  icon: React.ReactNode;
+  icon: string;
 };
 
 const defaultItems: ToolItem[] = [
   {
     name: "Python",
-    icon: <Image src="icons/python.png" alt="icon" width={24} height={24} />
+    icon: "/icons/python.png"
   },
   {
     name: "Azure-Blob",
-    icon: (
-      <Image src="/icons/azure-blob.png" alt="icon" width={24} height={24} />
-    )
+    icon: "/icons/azure-blob.png"
   },
   {
     name: "Databricks",
-    icon: (
-      <Image src="/icons/databricks.png" alt="icon" width={24} height={24} />
-    )
+    icon: "/icons/databricks.png"
   },
   {
     name: "R-Lang",
-    icon: (
-      <Image src="/icons/r-programming.png" alt="icon" width={24} height={24} />
-    )
+    icon: "/icons/r-programming.png"
   },
   {
     name: "PowerBi",
-    icon: <Image src="/icons/power-bi.png" alt="icon" width={24} height={24} />
+    icon: "/icons/power-bi.png"
   },
   {
     name: "SQL",
-    icon: <Image src="/icons/sql.png" alt="icon" width={24} height={24} />
+    icon: "/icons/sql.png"
   },
   {
     name: "Azure-SQL",
-    icon: (
-      <Image src="/icons/azure-sql.png" alt="Docker" width={24} height={24} />
-    )
-  },
-  {
-    name: "Python",
-    icon: <Image src="/icons/python.png" alt="icon" width={24} height={24} />
-  },
-  {
-    name: "Azure-Blob",
-    icon: (
-      <Image src="/icons/azure-blob.png" alt="icon" width={24} height={24} />
-    )
-  },
-  {
-    name: "Databricks",
-    icon: (
-      <Image src="/icons/databricks.png" alt="icon" width={24} height={24} />
-    )
-  },
-  {
-    name: "R-Lang",
-    icon: (
-      <Image src="/icons/r-programming.png" alt="icon" width={24} height={24} />
-    )
-  },
-  {
-    name: "PowerBi",
-    icon: <Image src="/icons/power-bi.png" alt="icon" width={24} height={24} />
-  },
-  {
-    name: "SQL",
-    icon: <Image src="/icons/sql.png" alt="icon" width={24} height={24} />
-  },
-  {
-    name: "Azure-SQL",
-    icon: (
-      <Image src="/icons/azure-sql.png" alt="Docker" width={24} height={24} />
-    )
+    icon: "/icons/azure-sql.png"
   }
 ];
 
@@ -109,7 +65,8 @@ export default function IconSlider() {
               key={i}
               className="flex-shrink-0 flex items-center gap-2 rounded-lg px-4 lg:px-9 py-2 bg-white/10 backdrop-blur-sm lg:backdrop-blur-3xl"
             >
-              {icon.icon}
+              {/* {icon.icon} */}
+              <Image src={icon.icon} alt="icon" width={24} height={24} />
               <span className="whitespace-nowrap">
                 {icon.name}
               </span>
