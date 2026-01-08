c88b94211425d88879d287e20c28349411cbedc1
omkute <omkute636@gmail.com>
2026-01-08 13:01:08 +0530

    Remover beta from text badge

--- app/(services)/businessinsights/page.tsx
+++ app/(services)/businessinsights/page.tsx
@@ -1,10 +1,20 @@
+import Image from 'next/image'
 import React from 'react'
 
 function BusinessInslights() {
   return (
-    <div className=' h-screen flex justify-center items-center gap-1'>
-        <h1> Business Inslights </h1>
-        <p className=' text-red-400'> Work in Progress !</p>
+    <div className=' h-screen flex flex-col  gap-1'>
+      <h1 className=' text-2xl text-center'> Business Inslights </h1>
+      <Image
+        src="/content/businessins.jpg"
+        height={1000}
+        width={1000}
+        alt='Business-Inslights'
+        className=' rounded-xl opacity-30'
+      />
+      <p className=' text-red-400'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic molestias nesciunt explicabo quod porro modi nobis mollitia eveniet consequuntur perferendis nisi, blanditiis delectus voluptas nemo quo labore! Accusamus consequuntur minus deserunt, eligendi placeat harum doloremque corrupti fugiat odit expedita at! Autem, necessitatibus. Quibusdam quos esse voluptatem in aliquid sapiente quod molestias non impedit, necessitatibus provident. Unde placeat labore neque repudiandae?
+      </p>
     </div>
   )
 }
--- components/costom/ShinyTextBadge.tsx
+++ components/costom/ShinyTextBadge.tsx
@@ -12,7 +12,7 @@ export function ShinyTextBadge() {
         )}
       >
         <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition cursor-none ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
-          <span>✨ Introducing SmartDataX Beta V1.0</span>
+          <span>✨ Introducing SmartDataX</span>
           < ChevronRight className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
         </AnimatedShinyText>
       </div>
